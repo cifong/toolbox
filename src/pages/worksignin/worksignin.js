21bd59bd582dec31810f10ee4feb7370334df3ba
cifong <raprope@gmail.com>
2024-10-26 13:00:49 +0800

    add dayoff parameter

--- src/pages/worksignin/worksignin.js
+++ src/pages/worksignin/worksignin.js
@@ -2,18 +2,19 @@ import React from "react";
 import { useStorage, useAuth } from "hooks/customhooks";
 
 function WorkTimeInfo(props) {
-    const { workingHours, workingMinutes, lunchBreakHours, lunchBreakMinutes, punchInTime } = props.data;
+    const { workingHours, workingMinutes, lunchBreakHours, lunchBreakMinutes, dayOffHours, dayOffMinutes, punchInTime } = props.data;
     if (!punchInTime) {
         return <></>;
     }
-    const totalWorkMinutes = workingHours * 60 + workingMinutes * 1;
+    const totalWorkMinutes = workingHours * 60 + workingMinutes * 1 - dayOffHours * 60 - dayOffMinutes * 1;
     const totalLunchBreakMinutes = lunchBreakHours * 60 + lunchBreakMinutes * 1;
     const getPunchOutTime = () => {
         const lunchBreakStart = new Date();
         lunchBreakStart.setHours(12, 0, 0);
         const punchoutTime = new Date(punchInTime);
-        if (punchInTime < lunchBreakStart) {
-            punchoutTime.setMinutes(punchoutTime.getMinutes() + totalWorkMinutes + totalLunchBreakMinutes);
+        punchoutTime.setMinutes(punchoutTime.getMinutes() + totalWorkMinutes);
+        if (totalWorkMinutes > 0 && punchoutTime >= lunchBreakStart) {
+            punchoutTime.setMinutes(punchoutTime.getMinutes() + totalLunchBreakMinutes);
             return punchoutTime;
         }
         const lunchBreakEnd = new Date(lunchBreakStart);
@@ -39,6 +40,8 @@ export default function Worksignin() {
         workingMinutes: 0,
         lunchBreakHours: 1,
         lunchBreakMinutes: 30,
+        dayOffHours: 0,
+        dayOffMinutes: 0,
         punchInTime: null
     });
     const handleSign = () => {
@@ -76,6 +79,13 @@ export default function Worksignin() {
                 <input type="number" name="lunchBreakHours" min="0" max="2" value={workTime.lunchBreakHours} onChange={handleConfig} />
                 <span>hours</span>
                 <input type="number" name="lunchBreakMinutes" min="0" max="59" value={workTime.lunchBreakMinutes} onChange={handleConfig} />
+                <span>minutes</span>
+            </div>
+            <div className="flex-row-center">
+                <span>DayOff:</span>
+                <input type="number" name="dayOffHours" min="0" max="8" value={workTime.dayOffHours} onChange={handleConfig} />
+                <span>hours</span>
+                <input type="number" name="dayOffMinutes" min="0" max="59" value={workTime.dayOffMinutes} onChange={handleConfig} />
                 <span>minutes</span>
             </div>
             <WorkTimeInfo data={workTime} />
